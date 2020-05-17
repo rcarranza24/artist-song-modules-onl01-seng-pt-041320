b4325e055227fc59dddf93e3045f8fffb2ad6dc7
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-17 20:11:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,35 @@
+require 'pry'
 
+
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+
+  @@artists = []
+
+  def initialize
+    super
+    @songs = []
+  end
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+end
