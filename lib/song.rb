505f1abb5fe07908d2005b098a98d112f5e505f0
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-17 20:12:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Song
+  attr_accessor :name
+  attr_reader :artist
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+
+  @@songs = []
+
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
+
+  def self.all
+    @@songs
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+end
